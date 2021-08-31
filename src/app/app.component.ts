d7243bbd44a9be75d805af1ab5f5108bf57f2b2f
santiago rouaux <sanrouaux@hotmail.com>
2021-08-31 16:51:51 -0300

    Ejercicio 1 resuelto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'santiago-rouaux-ejercicio1';
+  ageOne:string;
+  ageTwo:string;
+  sum:string;
+  average:string;
+
+  /**
+   * Constructor method
+   */
+  constructor() {
+    this.ageOne = '';
+    this.ageTwo = '';
+    this.sum = '';
+    this.average = '';
+  }
+
+  calculate() {
+    this.sum = String(parseInt(this.ageOne) + parseInt(this.ageTwo));
+    this.average = String((parseInt(this.ageOne) + parseInt(this.ageTwo)) / 2);
+  }
+
+  clean() {
+    this.ageOne = '';
+    this.ageTwo = '';
+    this.sum = '';
+    this.average = '';
+  }
+
 }
